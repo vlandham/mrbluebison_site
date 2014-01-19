87ae36ee62ce65440b5355742d5a76b3800b7a7a
Jim V <vlandham@gmail.com>
2014-01-18 21:22:25 -0800

    pouch and backpack!!!

--- image_creator.rb
+++ image_creator.rb
@@ -5,7 +5,7 @@ filename_base = File.basename(filename)
 
 `convert #{filename} -thumbnail '200x135>' -unsharp 0x.5 #{filename_base}_thumb.jpg`
 `convert #{filename} -thumbnail '200x135^' -unsharp 0x.5 -gravity center -extent '200x135' #{filename_base}_center_thumb.jpg`
-`convert #{filename} -resize '670x400^' -gravity center -extent '670x400' #{filename_base}_middle.jpg`
-`convert #{filename} -resize '670x400^' -gravity South -extent '670x400' #{filename_base}_bottom.jpg`
-`convert #{filename} -resize '670x400^' -gravity North -extent '670x400' #{filename_base}_top.jpg`
-`convert #{filename} -resize '670x400^' -size 670x400 -gravity center -crop 670x400 #{filename_base}_crop.jpg`
+`convert #{filename} -resize '800x480^' -gravity center -extent '800x480' #{filename_base}_middle.jpg`
+`convert #{filename} -resize '800x480^' -gravity South -extent '800x480' #{filename_base}_bottom.jpg`
+`convert #{filename} -resize '800x480^' -gravity North -extent '800x480' #{filename_base}_top.jpg`
+`convert #{filename} -resize '800x480^' -size 800x480 -gravity center -crop 800x480 #{filename_base}_crop.jpg`
